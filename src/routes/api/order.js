62e827ff99cc0156a621b925f02f15574aac6817
zakinghaniweb <zakinghani1@gmail.com>
2025-11-27 19:59:39 +0600

    final

--- src/routes/api/order.js
+++ src/routes/api/order.js
@@ -2,6 +2,6 @@ const express = require("express")
 const { orderRegister } = require("../../controllers/orderController");
 const orderRouter = express.Router()
 
-orderRouter.post("/orderRegister", orderRegister);
+orderRouter.get("/orderRegister", orderRegister);
 
 module.exports = orderRouter;
